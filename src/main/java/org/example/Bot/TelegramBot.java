cd4490f17b542ba41d4d86ae971239af9c7c313a
aklana <mashachircova2@gmail.com>
2024-09-30 23:40:52 +0500

    зависимости

---
+++ src/main/java/org/example/Bot/TelegramBot.java
@@ -0,0 +1,73 @@
+package org.example.Bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "ReverseEsreveR_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7529659379:AAHmTD0Du91cIPRQOcQuTjCZdM2PR6uv2MA";
+    }
+    private void sendMenu(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выберите действие:");
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setSelective(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Вариант 1");
+        row.add("Вариант 2");
+        keyboard.add(row);
+        keyboardMarkup.setKeyboard(keyboard);
+
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (messageText.equals("/start")) {
+                sendMenu(chatId);
+            }
+            else if (messageText.equals("Вариант 1")) {
+                SendMessage message = new SendMessage();
+                message.setChatId(String.valueOf(chatId));
+                message.setText("Ты крутой");
+                try {
+                    execute(message); // Отправляем сообщение
+                } catch (Exception e) {
+                    e.printStackTrace(); // Обрабатываем возможные исключения
+                }
+            }
+        }
+    }
+}
