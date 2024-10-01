468d1bd70306d014e32b7afff068d62812d55696
aklana <mashachircova2@gmail.com>
2024-10-01 14:02:21 +0500

    класс TarotCard

--- src/main/java/org/example/Bot/TelegramBot.java
+++ src/main/java/org/example/Bot/TelegramBot.java
@@ -9,9 +9,9 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
@@ -35,8 +35,8 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         List<KeyboardRow> keyboard = new ArrayList<>();
         KeyboardRow row = new KeyboardRow();
-        row.add("Вариант 1");
-        row.add("Вариант 2");
+        row.add("Изучение карт");
+        row.add("Ежедневное предсказание");
         keyboard.add(row);
         keyboardMarkup.setKeyboard(keyboard);
 
@@ -51,6 +51,14 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+            TarotCards Cards;
+        try {
+            Cards = new TarotCards("text.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        SendMessage message = new SendMessage();
+
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
@@ -58,10 +66,37 @@ public class TelegramBot extends TelegramLongPollingBot {
             if (messageText.equals("/start")) {
                 sendMenu(chatId);
             }
-            else if (messageText.equals("Вариант 1")) {
-                SendMessage message = new SendMessage();
+            else if (messageText.equals("Изучение карт")) {
                 message.setChatId(String.valueOf(chatId));
-                message.setText("Ты крутой");
+                message.setText("Кубки \n"+
+                        "Мечи \n" +
+                        "Жезлы \n" +
+                        "Пентакли \n"+
+                        "Старшие арканы");
+                try {
+                    execute(message); // Отправляем сообщение
+                } catch (Exception e) {
+                    e.printStackTrace(); // Обрабатываем возможные исключения
+                }
+            }
+            else if (messageText.equals("Мечи")) {
+                message.setChatId(String.valueOf(chatId));
+                message.setText("Туз мечей \n"+
+                        "2 мечей \n" +
+                        "3 мечей \n" +
+                        "4 мечей \n"+
+                        "5 мечей \n" +
+                        "6 мечей \n" +
+                        "7 мечей \n");
+                try {
+                    execute(message); // Отправляем сообщение
+                } catch (Exception e) {
+                    e.printStackTrace(); // Обрабатываем возможные исключения
+                }
+            }//либо хэш мэп, либо подгрузка из ткст файла. грай кэтч своя функция
+            else if (messageText.equals("Туз мечей")) {
+                message.setChatId(String.valueOf(chatId));
+                message.setText(Cards.get("Туз мечей"));
                 try {
                     execute(message); // Отправляем сообщение
                 } catch (Exception e) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.Bot.TarotCard;
+import org.example.Bot.TarotCards;
 import org.example.Bot.TelegramBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -8,14 +8,20 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.FileNotFoundException;
 
-//public class Main {
-    //public static void main(String[] args) throws TelegramApiException {
-      //  TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        //telegramBotsApi.registerBot(new TelegramBot());
-  //  }
-//}
-public class Main{
-    public static void main(String[] args) throws FileNotFoundException {
-        TarotCard name=new TarotCard("text.txt");
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TelegramBot());
+        try {
+            TarotCards name=new TarotCards("text.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+//public class Main{
+   // public static void main(String[] args) throws FileNotFoundException {
+      //  TarotCards name=new TarotCards("text.txt");
+
+   // }
+//}
